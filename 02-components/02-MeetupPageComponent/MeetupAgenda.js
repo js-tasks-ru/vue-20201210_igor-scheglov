79e773303da6faa195902afcb3172a4f0b97f24d
Scheglov Igor <scheglov.igor@gmail.com>
2021-01-06 03:20:55 +0300

    HW 02-02

--- 02-components/02-MeetupPageComponent/MeetupAgenda.js
+++ 02-components/02-MeetupPageComponent/MeetupAgenda.js
@@ -1,14 +1,24 @@
-// import { MeetupAgendaItem } from './MeetupAgendaItem.js';
+import { MeetupAgendaItem } from './MeetupAgendaItem.js';
 
 export const MeetupAgenda = {
   name: 'MeetupAgenda',
 
   template: `
     <div class="meetup-agenda">
-      <div class="meetup-agenda__item"></div>
+      <p v-if="agenda.empty" class="meetup-agenda__empty">Программа пока пуста, но когда-нибудь в ней обязательно что-нибудь появится!</p>
+      <meetup-agenda-item v-for="agendaItem in agenda" :agendaItem="agendaItem" :key="agendaItem.id"/>
     </div>`,
 
   // components
+  components: {
+    MeetupAgendaItem,
+  },
 
   // props
+  props: {
+    agenda: {
+      type: Array,
+      required: true,
+    },
+  },
 };
--- 02-components/02-MeetupPageComponent/MeetupPage.js
+++ 02-components/02-MeetupPageComponent/MeetupPage.js
@@ -1,16 +1,28 @@
-// import { MeetupView } from './MeetupView.js';
-// import { MEETUP_ID, fetchMeetup } from './data.js';
+import { MeetupView } from './MeetupView.js';
+import { MEETUP_ID, fetchMeetup } from './data.js';
 
-/*export const MeetupPage = {
+export const MeetupPage = {
   name: 'MeetupPage',
 
-  template: `<div>???</div>`,
+  template: `<meetup-view v-if="meetup" :meetup="meetup"></meetup-view>`,
 
   // components
+  components: {
+    MeetupView,
+  },
 
   // data
+  data() {
+    return {
+      meetup: null,
+    };
+  },
 
   // mounted
+  async mounted() {
+    // Требуется получить данные митапа с API
+    this.meetup = await fetchMeetup(MEETUP_ID);
+  },
 
   // methods
-};*/
+};
